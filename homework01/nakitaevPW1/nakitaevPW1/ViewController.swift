b3a7bb3825125890ef0eac17f0f173fd9804f0d5
Nikita Kitaev <nik_kitaev@mail.ru>
2023-09-21 20:07:03 +0300

    Second homework added

---
+++ homework01/nakitaevPW1/nakitaevPW1/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  nakitaevPW1
+//
+//  Created by Никита on 18.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet var views: [UIView]!
+    
+    // MARK: Function activates on complited view load.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: Function changes color and border radius on button pressed.
+    
+    @IBAction func buttonWasPressed(_ sender: Any) {
+        for view in views {
+            changeColor(view: view)
+            changeCornerReduis(view: view)
+        }
+    }
+    
+    // MARK: Function returns a random color.
+    
+    func getUniqueColor() -> UIColor {
+        var color = UIColor(
+                red: .random(in: 0...1),
+                green: .random(in: 0...1),
+                blue: .random(in: 0...1),
+                alpha: 1
+            )
+        
+        return color
+    }
+    
+    // MARK: Function changes view's color.
+    
+    func changeColor(view: UIView) {
+        view.backgroundColor = getUniqueColor()
+    }
+    
+    // MARK: Function changes view's border radius.
+    
+    func changeCornerReduis(view: UIView, maxRadius: CGFloat = 25) {
+        view.layer.cornerRadius = .random(in: 0...maxRadius)
+    }
+}
+
